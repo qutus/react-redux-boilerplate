e1f129647ebbf7546c8766f2a78bb5a8d9647d0a
qutus <qrouault@gmail.com>
2018-03-24 16:07:29 +0100

    Set up tests

---
+++ src/App.test.js
@@ -0,0 +1,22 @@
+import React from 'React';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+
+  describe('* render', () => {
+
+    it('Should render App', () => {
+
+      const wrapper = shallow(<App />);
+      expect(wrapper.find('div')).toBeTruthy();
+
+    });
+
+  });
+
+});
